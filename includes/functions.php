26ed52daa095b83a7b41006e94021a9943de23cc
bhattacharyyadipra-droid <bhattacharyyadipra@gmail.com>
2026-04-10 00:01:16 +0530

    Create includes/functions.php for utility functions related to product, cart, and user management

---
+++ includes/functions.php
@@ -0,0 +1,70 @@
+<?php
+
+// Utility Functions for Product Management
+
+/**
+ * Function to get product details by ID
+ * @param int $id
+ * @return array
+ */
+function getProductById($id) {
+    // Implementation goes here
+}
+
+/**
+ * Function to get all products
+ * @return array
+ */
+function getAllProducts() {
+    // Implementation goes here
+}
+
+// Utility Functions for Cart Operations
+
+/**
+ * Function to add item to cart
+ * @param int $productId, int $quantity
+ * @return void
+ */
+function addToCart($productId, $quantity) {
+    // Implementation goes here
+}
+
+/**
+ * Function to remove item from cart
+ * @param int $productId
+ * @return void
+ */
+function removeFromCart($productId) {
+    // Implementation goes here
+}
+
+/**
+ * Function to get cart items
+ * @return array
+ */
+function getCartItems() {
+    // Implementation goes here
+}
+
+// Utility Functions for User Authentication
+
+/**
+ * Function to register a user
+ * @param array $userData
+ * @return void
+ */
+function registerUser($userData) {
+    // Implementation goes here
+}
+
+/**
+ * Function to authenticate a user
+ * @param string $username, string $password
+ * @return bool
+ */
+function authenticateUser($username, $password) {
+    // Implementation goes here
+}
+
+?>
