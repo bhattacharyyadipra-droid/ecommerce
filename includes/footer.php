4e3c587b0a0ddfc78a13611a6cbb536924700ec3
bhattacharyyadipra-droid <bhattacharyyadipra@gmail.com>
2026-04-10 00:01:26 +0530

    Create footer.php with footer navigation, social links, and contact information

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<footer>
+  <nav>
+    <ul>
+      <li><a href="/home">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/services">Services</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+  <div class="social-links">
+    <a href="https://facebook.com/yourprofile">Facebook</a>
+    <a href="https://twitter.com/yourprofile">Twitter</a>
+    <a href="https://instagram.com/yourprofile">Instagram</a>
+  </div>
+  <div class="contact-info">
+    <p>Email: contact@yourdomain.com</p>
+    <p>Phone: +123456789</p>
+  </div>
+  <p>&copy; 2026 Your Company. All rights reserved.</p>
+</footer>
